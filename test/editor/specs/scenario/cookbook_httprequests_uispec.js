7c1d39e7c3f473291b4c6041b6438bc33118d3b8
ZafeerCodes <mohammed.xafeer@gmail.com>
2025-02-17 14:15:51 +0530

    try

--- test/editor/specs/scenario/cookbook_httprequests_uispec.js
+++ test/editor/specs/scenario/cookbook_httprequests_uispec.js
@@ -88,7 +88,7 @@ describe('cookbook', function () {
 
             debugTab.open();
             injectNode.clickLeftButton();
-            debugTab.getMessage().should.containEql('<title>Masterworks again sudo</title>');
+            debugTab.getMessage().should.containEql('<title>Masterworks again will</title>');
         });
 
         it('set the URL of a request using a template', function () {
